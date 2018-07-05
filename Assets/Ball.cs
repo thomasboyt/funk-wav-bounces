7828fe898e1e73d0e9af9e41c3b93e206e601ff3
Thomas Boyt <me@thomasboyt.com>
2018-07-05 03:45:38 -0400

    Prototype

---
+++ Assets/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public GameObject player;
+    private Transform originalParent;
+    private Rigidbody rb;
+    //private bool caught = false;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        originalParent = transform.parent;
+        //GetComponent<Rigidbody>().velocity = 10 * transform.localScale.x * transform.forward;
+        CaughtByPlayer();
+	}
+
+    public void CaughtByPlayer() {
+        // player takes ownership of ball position
+        rb.isKinematic = true;
+        //caught = true;
+        transform.SetParent(player.transform);
+        transform.localPosition = new Vector3(0, 1f, 0);
+        transform.rotation = player.transform.rotation;
+    }
+
+    public void Throw() {
+        // move ball in front of player so player + ball dont collide
+        transform.localPosition = transform.forward * 2f;
+        rb.isKinematic = false;
+        rb.velocity = transform.forward * 30f;
+        transform.SetParent(originalParent);
+    }
+
+    //void OnCollisionEnter(Collision otherObj) {
+    //    if (otherObj.gameObject.name == "Player") {
+    //        CaughtByPlayer();
+    //    }
+    //}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        var moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+        if (Input.GetKey(KeyCode.Space)) {
+            // throw ball
+            var ball = transform.Find("Ball");
+            if (ball) {
+                ball.GetComponent<Ball>().Throw();
+            }
+        }
+        if (Input.GetKey(KeyCode.R)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        
+        GetComponent<CharacterController>().Move(moveDirection * Time.deltaTime);
+    }
+
+    // called when player is moving
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.name == "Ball")
+        {
+            hit.gameObject.GetComponent<Ball>().CaughtByPlayer();
+        }
+    }
+
+    // called when player isn't moving
+    void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.name == "Ball")
+        {
+            hit.gameObject.GetComponent<Ball>().CaughtByPlayer();
+        }
+    }
+}
